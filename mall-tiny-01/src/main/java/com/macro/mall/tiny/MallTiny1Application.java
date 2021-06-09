7e13af68c36dbaed1d8b10e48c016b488f33fdb4
zhaoyijun1551 <zhj1551@163.com>
2021-06-09 23:28:38 +0800

    mybatis自动生成器

---
+++ mall-tiny-01/src/main/java/com/macro/mall/tiny/MallTiny1Application.java
@@ -0,0 +1,11 @@
+package com.macro.mall.tiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallTiny1Application {
+    public static void main(String[] args) {
+        SpringApplication.run(MallTiny1Application.class,args);
+    }
+}
---
+++ mall-tiny-01/src/main/java/com/macro/mall/tiny/mbg/mapper/CmsSubjectCommentMapper.java
@@ -0,0 +1,30 @@
+package com.macro.mall.tiny.mbg.mapper;
+
+import com.macro.mall.tiny.mbg.model.CmsSubjectComment;
+import com.macro.mall.tiny.mbg.model.CmsSubjectCommentExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface CmsSubjectCommentMapper {
+    int countByExample(CmsSubjectCommentExample example);
+
+    int deleteByExample(CmsSubjectCommentExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(CmsSubjectComment record);
+
+    int insertSelective(CmsSubjectComment record);
+
+    List<CmsSubjectComment> selectByExample(CmsSubjectCommentExample example);
+
+    CmsSubjectComment selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") CmsSubjectComment record, @Param("example") CmsSubjectCommentExample example);
+
+    int updateByExample(@Param("record") CmsSubjectComment record, @Param("example") CmsSubjectCommentExample example);
+
+    int updateByPrimaryKeySelective(CmsSubjectComment record);
+
+    int updateByPrimaryKey(CmsSubjectComment record);
+}
