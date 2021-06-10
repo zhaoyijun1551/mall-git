1ab605561c60db9dd421c24e76b0d0f3cd7e62b8
zhaoyijun1551 <zhj1551@163.com>
2021-06-10 23:40:54 +0800

    swagger整合

--- mall-tiny-01/src/main/java/com/macro/mall/tiny/mbg/model/UmsMemberProductCategoryRelation.java
+++ mall-tiny-01/src/main/java/com/macro/mall/tiny/mbg/model/UmsMemberProductCategoryRelation.java
@@ -1,5 +1,6 @@
 package com.macro.mall.tiny.mbg.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import java.io.Serializable;
 
 public class UmsMemberProductCategoryRelation implements Serializable {
--- mall-tiny-01/src/main/java/com/macro/mall/tiny/service/impl/PmsBrandServiceImpl.java
+++ mall-tiny-01/src/main/java/com/macro/mall/tiny/service/impl/PmsBrandServiceImpl.java
@@ -16,6 +16,7 @@ import java.util.List;
 @Service
 public class PmsBrandServiceImpl implements PmsBrandService {
 
+
     @Autowired
     private PmsBrandMapper brandMapper;
     @Override
