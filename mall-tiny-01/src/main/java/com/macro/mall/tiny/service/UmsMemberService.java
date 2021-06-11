c6c22fddd7df68e8fc1d4ba00dc6c81abdf0901c
zhaoyijun1551 <zhj1551@163.com>
2021-06-11 23:47:07 +0800

    整合redis，验证码接口测试

---
+++ mall-tiny-01/src/main/java/com/macro/mall/tiny/service/UmsMemberService.java
@@ -0,0 +1,18 @@
+package com.macro.mall.tiny.service;
+
+import com.macro.mall.tiny.common.api.CommonResult;
+
+/**
+ * 会员管理Service
+ */
+public interface UmsMemberService {
+    /**
+     * 生成验证码
+     */
+    CommonResult generateAuthCode(String telephone);
+
+    /**
+     * 判断验证码和手机号码是否匹配
+     */
+    CommonResult verifyAuthCode(String telephone, String authCode);
+}
