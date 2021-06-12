ac9ccdc962efecec8ef1e4aa57aeeaf16271b2a2
zhaoyijun1551 <zhj1551@163.com>
2021-06-12 22:07:45 +0800

    整合SpringTask定时任务

---
+++ mall-tiny-01/src/main/java/com/macro/mall/tiny/config/SpringTaskConfig.java
@@ -0,0 +1,12 @@
+package com.macro.mall.tiny.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 定时任务配置
+ */
+@Configuration
+@EnableScheduling
+public class SpringTaskConfig {
+}
