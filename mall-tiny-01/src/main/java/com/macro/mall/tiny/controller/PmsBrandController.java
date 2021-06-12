61c363da8f897c53d030ce26faf5acf0fdc0677e
zhaoyijun1551 <zhj1551@163.com>
2021-06-12 20:08:16 +0800

    mall整合SpringSecurity和JWT实现认证和授权
    注册功能已验证，登录功能报空指针

--- mall-tiny-01/src/main/java/com/macro/mall/tiny/controller/PmsBrandController.java
+++ mall-tiny-01/src/main/java/com/macro/mall/tiny/controller/PmsBrandController.java
@@ -9,6 +9,7 @@ import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -28,6 +29,7 @@ public class PmsBrandController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PmsBrandController.class);
 
+    @PreAuthorize("hasAuthority('pms:brand:read')")
     @ApiOperation("获取所有品牌列表")
     @RequestMapping(value = "listAll", method = RequestMethod.GET)
     @ResponseBody
@@ -35,6 +37,8 @@ public class PmsBrandController {
         return CommonResult.success(demoService.listAllBrand());
     }
 
+
+    @PreAuthorize("hasAuthority('pms:brand:create')")
     @ApiOperation("添加品牌")
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     @ResponseBody
@@ -51,6 +55,7 @@ public class PmsBrandController {
         return commonResult;
     }
 
+    @PreAuthorize("hasAuthority('pms:brand:update')")
     @ApiOperation("更新指定id品牌信息")
     @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
     @ResponseBody
@@ -67,6 +72,7 @@ public class PmsBrandController {
         return commonResult;
     }
 
+    @PreAuthorize("hasAuthority('pms:brand:delete')")
     @ApiOperation("删除指定id的品牌")
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
     @ResponseBody
---
+++ mall-tiny-01/src/main/java/com/macro/mall/tiny/service/impl/UmsAdminServiceImpl.java
@@ -0,0 +1,103 @@
+package com.macro.mall.tiny.service.impl;
+
+import com.macro.mall.tiny.common.utils.JwtTokenUtil;
+import com.macro.mall.tiny.dao.UmsAdminRoleRelationDao;
+import com.macro.mall.tiny.mbg.mapper.UmsAdminMapper;
+import com.macro.mall.tiny.mbg.model.UmsAdmin;
+import com.macro.mall.tiny.mbg.model.UmsAdminExample;
+import com.macro.mall.tiny.mbg.model.UmsPermission;
+import com.macro.mall.tiny.service.UmsAdminService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * UmsAdminService实现类
+ */
+
+@Service
+public class UmsAdminServiceImpl implements UmsAdminService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UmsAdminServiceImpl.class);
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Value("${jwt.tokenHead}")
+    private String tokenHead;
+    @Autowired
+    private UmsAdminMapper adminMapper;
+    /*@Autowired(required=false)：表示忽略当前要注入的bean，如果有直接注入，没有跳过，不会报错。
+    在容器的启动过程中，会初始化很多bean，这也是spring的核心之一（IOC）。但是在注入的过程中，扫描到公共方法中要注入的bean，并未找到，强行注入就会注入失败。
+    我们又不能单独的去除改方法，所以我们采取的思想就是有bean就注入，没有就不注入。
+    */
+    @Autowired(required = false)
+    private UmsAdminRoleRelationDao adminRoleRelationDao;
+
+    @Override
+    public UmsAdmin getAdminByUsername(String username) {
+        UmsAdminExample example = new UmsAdminExample();
+        example.createCriteria().andUsernameEqualTo(username);
+        List<UmsAdmin> adminList = adminMapper.selectByExample(example);
+        if (adminList != null && adminList.size() > 0) {
+            return adminList.get(0);
+        }
+        return null;
+    }
+
+    @Override
+    public UmsAdmin register(UmsAdmin umsAdminParam) {
+        UmsAdmin umsAdmin = new UmsAdmin();
+        BeanUtils.copyProperties(umsAdminParam, umsAdmin);
+        umsAdmin.setCreateTime(new Date());
+        umsAdmin.setStatus(1);
+        //查询是否有相同用户名的用户
+        UmsAdminExample example = new UmsAdminExample();
+        example.createCriteria().andUsernameEqualTo(umsAdmin.getUsername());
+        List<UmsAdmin> umsAdminList = adminMapper.selectByExample(example);
+        if (umsAdminList.size() > 0) {
+            return null;
+        }
+        //将密码进行加密操作
+        String encodePassword = passwordEncoder.encode(umsAdmin.getPassword());
+        umsAdmin.setPassword(encodePassword);
+        adminMapper.insert(umsAdmin);
+        return umsAdmin;
+    }
+
+    @Override
+    public String login(String username, String password) {
+        String token = null;
+        try {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            if (!passwordEncoder.matches(password, userDetails.getPassword())) {
+                throw new BadCredentialsException("密码不正确");
+            }
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            token = jwtTokenUtil.generateToken(userDetails);
+        } catch (AuthenticationException e) {
+            LOGGER.warn("登录异常:{}", e.getMessage());
+        }
+        return token;
+    }
+    @Override
+    public List<UmsPermission> getPermissionList(Long adminId) {
+        return adminRoleRelationDao.getPermissionList(adminId);
+    }
+}
