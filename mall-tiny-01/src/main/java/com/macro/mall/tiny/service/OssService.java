c33eaef38bea6a10df9c6afec5bb583e68fee171
zhaoyijun1551 <zhj1551@163.com>
2021-06-14 16:04:07 +0800

    SSO文件上传

---
+++ mall-tiny-01/src/main/java/com/macro/mall/tiny/service/OssService.java
@@ -0,0 +1,21 @@
+package com.macro.mall.tiny.service;
+
+import com.macro.mall.tiny.dto.OssCallbackResult;
+import com.macro.mall.tiny.dto.OssPolicyResult;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * oss上传管理Service
+ */
+public interface OssService {
+    /**
+     * oss上传策略生成
+     */
+    OssPolicyResult policy();
+
+    /**
+     * oss上传成功回调
+     */
+    OssCallbackResult callback(HttpServletRequest request);
+}
